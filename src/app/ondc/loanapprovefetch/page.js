fbab9ebe6cff1b3b6c5a926d64270963098a0501
Rahul <rahulgupta.credithaat@gmail.com>
2025-09-12 15:30:49 +0530

    added suspense to loanapprovalpagedataload page.js

--- src/app/ondc/loanapprovefetch/page.js
+++ src/app/ondc/loanapprovefetch/page.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, {Suspense} from 'react'
 import LoanApprovalPageDataLoad from "@/component/Rysa/ONDC/LoadingPages/LoanApprovalPageDataLoad";
 
 const page = () => {
   return (
     <>
+    <Suspense fallback={<div>Loading...</div>}>
     <LoanApprovalPageDataLoad/>
+    </Suspense>
     </>
   )
 }
