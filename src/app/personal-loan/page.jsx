cdc938652af5be925d9f1de957342e519aa38898
tejas-deshmukh23 <yogitatekale0802@gmail.com>
2025-09-16 16:22:36 +0530

    Rejection page redirect on embedded list from v3 api and changes in url name on pl-journey and personal-loan

--- src/app/personal-loan/page.jsx
+++ src/app/personal-loan/page.jsx
@@ -1,24 +1,31 @@
 'use client'
 import React from 'react'
-import { Roboto } from 'next/font/google';
-import RysaForm1 from '../../component/Rysa/rysaform/RysaNewPage';
-import PlOTPBottomSheet from '@/component/Rysa/rysaform/NewPlOtpBottomSheet/PlOTPBottomSheet';
+import { Outfit } from "next/font/google";
+import PersonalDetailPage from "../../component/NewJourney/personalDetailePage";
+import PersonalDetailPage2 from "../../component/NewJourney/personalDetailePage2";
+import PersonalDetailPage3 from "../../component/NewJourney/personalDetailePage3";
+import FirstPage from '@/component/NewJourney/firstpage';
 
-const roboto = Roboto({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-  display: 'swap',
+const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
 });
-function page() { 
+function page() {
+  // Dummy company object (तू backend मधून फक्त structure असाच ठेव)
+
+
   return (
-    <div className={roboto.className}>
+    <div className={outfit.className}>
       <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
-          />
-  
-      <RysaForm1 />
-      {/* <PlOTPBottomSheet/> */}
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+      />
+      
+      {/* <PersonalDetailPage /> */}
+      {/* <PersonalDetailPage2 /> */}
+      {/* <PersonalDetailPage3 /> */}
+      <FirstPage/>
     </div>
   )
 }
