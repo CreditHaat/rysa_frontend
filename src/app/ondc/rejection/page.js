1987427badd5209c22efd8e004ecf542fb77adb8
Rahul <rahulgupta.credithaat@gmail.com>
2025-09-16 17:35:44 +0530

    added suspense in rejection page.js

--- src/app/ondc/rejection/page.js
+++ src/app/ondc/rejection/page.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React,{Suspense} from 'react'
 // import NewRejectionPage from "@/component/Yubi/newrejectionpage"
 import RejectionPage from '@/component/Rysa/ONDC/LoadingPages/rejectionpage'
 
 const page = () => {
   return (
     <>
+    <Suspense fallback={<></>}>
        <RejectionPage/> 
+       </Suspense>
     </>
   )
 }
